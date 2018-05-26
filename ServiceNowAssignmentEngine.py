dba84b2bc376656841e2a696be2ef93da7fab273
owen-cody <OwenCCody@gmail.com>
2018-05-25 23:03:35 -0500

    ServiceNowAssignmentEngine.py

--- ServiceNowAssignmentEngine.py
+++ ServiceNowAssignmentEngine.py
@@ -1,6 +1,12 @@
 #Python
-import smtplib, sqlite3
+# Owen Cody & Deborah Cody, 5-25-2018, Python
 
+# Overview :
+# ServiceNowAssignmentEngine was created as a program with the intent of sending
+# eMail notifications to a random recipient stored in an sqlite3 database
+# when a change is detected in a SQL Database
+
+import smtplib, sqlite3
 
 def sendMailUpdate():
     frUsr = str(input("please input your username!     "))
