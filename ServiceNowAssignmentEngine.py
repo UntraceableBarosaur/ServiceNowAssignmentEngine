2955527cb55f71ee77c6cfaa19009f5a8affcc13
owen-cody <OwenCCody@gmail.com>
2018-05-25 22:58:06 -0500

    ServiceNowAssignmentEngine.py

--- ServiceNowAssignmentEngine.py
+++ ServiceNowAssignmentEngine.py
@@ -1,12 +1,15 @@
 #Python
-import smtplib
-frUsr = str(input("please input your username!     "))
-pswd = str(input("please input your password!     "))
-toUsr = str(input("please input your the desired recipient!     "))
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(frUsr, pswd)
+import smtplib, sqlite3
 
-msg = "You Have Mail!!"
-server.sendmail(frUsr, toUsr, msg)
-server.quit()
+
+def sendMailUpdate():
+    frUsr = str(input("please input your username!     "))
+    pswd = str(input("please input your password!     "))
+    toUsr = str(input("please input your the desired recipient!     "))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(frUsr, pswd)
+
+    msg = "You Have Mail!!"
+    server.sendmail(frUsr, toUsr, msg)
+    server.quit()
